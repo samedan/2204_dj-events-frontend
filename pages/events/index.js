f664402f57ade34f4c0f2fc687835f7a0fd4e32d
Daniel Popescu <same.dan@gmail.com>
2022-04-28 17:26:09 +0200

    getStaticProps

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,22 +1,16 @@
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
-import Link from "next/link";
-import EventItem from "./../components/EventItem";
+import EventItem from "@/components/EventItem";
 
-export default function HomePage({ events }) {
+export default function EventsPage({ events }) {
   console.log(events);
   return (
     <Layout>
-      <h1>Upcoming Events</h1>
+      <h1>Events</h1>
       {events.length === 0 && <h3>No events yet</h3>}
       {events.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
-      {events.length > 0 && (
-        <Link href="/events">
-          <a className={"btn-secondary"}>View All Events</a>
-        </Link>
-      )}
     </Layout>
   );
 }
@@ -26,7 +20,7 @@ export async function getStaticProps() {
   const events = await res.json();
 
   return {
-    props: { events: events.slice(0, 3) },
+    props: { events },
     revalidate: 1, // seconds to ckeck if data changes on getStaticProps
   };
 }
